58a5e6bacecafa75e38e103d0ed96480c3944f33
Your Name <you@example.com>
2022-01-31 16:56:46 +0200

    hgjhhghj

---
+++ build/precache-manifest.9ff4ccb93d81bce5035a4b24c89eba44.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed15122624bc41ef6f72",
+    "url": "/bookslist/static/css/main.ac1d25d9.chunk.css"
+  },
+  {
+    "revision": "ed15122624bc41ef6f72",
+    "url": "/bookslist/static/js/main.ed151226.chunk.js"
+  },
+  {
+    "revision": "a9012fae4f470ac29aa1",
+    "url": "/bookslist/static/js/1.a9012fae.chunk.js"
+  },
+  {
+    "revision": "98a761a49b40fcbbc04c",
+    "url": "/bookslist/static/js/runtime~main.98a761a4.js"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/bookslist/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/bookslist/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/bookslist/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "1564221e814ee747c577d00acedf6a63",
+    "url": "/bookslist/index.html"
+  }
+];
